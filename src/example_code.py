224fbd4b90cb4aa27337dcf22aa3bc1a057694de
SarahDavis9 <davi8874@vandals.uidaho.edu>
2025-06-10 12:40:13 -0700

    Edited sample code

--- src/example_code.py
+++ src/example_code.py
@@ -7,13 +7,13 @@ def main():
     position, orientation = sb.get_cartesian_position()
     print(f"Cartesian Position: {position}, Orientation: {orientation}")
 
-    # Move the robot using joint rotations
-    j1, j2, j3, j4, j5, j6 = sb.get_joint_position()
-    print(f"Joint Positions: {j1}, {j2}, {j3}, {j4}, {j5}, {j6}")
-
     # Move the Z position 80mm up
     sb.move_cartesian(position.x, position.y, position.z + 0.08)
 
+    # Move the robot using joint rotations
+    j1, j2, j3, j4, j5, j6 = sb.get_joint_position()
+    print(f"Joint Positions: {j1}, {j2}, {j3}, {j4}, {j5}, {j6}")
+    
     # Rotate the last joint 90 degrees
     sb.move_joint(j1, j2, j3, j4, j5, j6 + 1.57)
     
